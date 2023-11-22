45e56c5c7b8ac603f58a3bc01e0c1aa52a6feca2
void2610 <izumisan2610@gmail.com>
2023-11-22 15:21:04 +0900

    カメラ修正

--- Assets/Scripts/PerformanceCamera.cs
+++ Assets/Scripts/PerformanceCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class PerformanceCamera : MonoBehaviour
 {
@@ -8,12 +9,46 @@ public class PerformanceCamera : MonoBehaviour
     private GameObject player;
     [SerializeField]
     private Vector3 offset;
-    void Start()
-    {
 
+    private Camera captureCamera;
+    private string savePath = "Assets/Resources/Screenshots/";
+
+    public Texture2D Capture()
+    {
+        // カメラのレンダーテクスチャを作成
+        RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        captureCamera.targetTexture = renderTexture;
+
+        // テクスチャをアクティブにしてレンダリング
+        captureCamera.Render();
+        RenderTexture.active = renderTexture;
+
+        // テクスチャから2Dテクスチャを作成
+        Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenshot.Apply();
+
+        // レンダーテクスチャとアクティブなテクスチャをリセット
+        captureCamera.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(renderTexture);
+
+        // 画像を保存
+        byte[] bytes = screenshot.EncodeToPNG();
+        string fileName = "Screenshot_" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+        string filePath = Path.Combine(savePath, fileName);
+        File.WriteAllBytes(filePath, bytes);
+
+        Debug.Log("Screenshot saved to: " + filePath);
+
+        return screenshot;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        captureCamera = this.GetComponent<Camera>();
+    }
+
     void Update()
     {
         transform.position = player.transform.position + offset;
--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -1,58 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
 public class TakePicture : MonoBehaviour
 {
     [SerializeField]
     private GameObject performanceCamera;
-
-    private Camera captureCamera;
-
-    private string savePath = "Assets/Resources/Screenshots/";
-
-    void Capture()
-    {
-        // カメラのレンダーテクスチャを作成
-        RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        captureCamera.targetTexture = renderTexture;
-
-        // テクスチャをアクティブにしてレンダリング
-        captureCamera.Render();
-        RenderTexture.active = renderTexture;
-
-        // テクスチャから2Dテクスチャを作成
-        Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        screenshot.Apply();
-
-        // レンダーテクスチャとアクティブなテクスチャをリセット
-        captureCamera.targetTexture = null;
-        RenderTexture.active = null;
-        Destroy(renderTexture);
-
-        // 画像を保存
-        byte[] bytes = screenshot.EncodeToPNG();
-        string fileName = "Screenshot_" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
-        string filePath = Path.Combine(savePath, fileName);
-        File.WriteAllBytes(filePath, bytes);
-
-        Debug.Log("Screenshot saved to: " + filePath);
-    }
-
-
     void Start()
     {
-        captureCamera = performanceCamera.GetComponent<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            Capture();
+            Texture2D screenshot = performanceCamera.GetComponent<PerformanceCamera>().Capture();
         }
     }
 }
