131865dc2b2cc3890511c8ec70b89abb9e08eece
nari88118 <109565861+nari88118@users.noreply.github.com>
2023-11-24 11:32:50 +0900

    PRの修正

--- Assets/Scripts/PlayerFoot.cs
+++ Assets/Scripts/PlayerFoot.cs
@@ -9,21 +9,23 @@ public class PlayerFoot : MonoBehaviour
     private int triggerValue = 0;
     
     //Vector3 velocity;
-    Vector3 velocity;
-    Vector3 direction;
-    Vector3 previousPosition; 
-    Vector3 previousVelocity;
-    float forceMagnitude;
-   
+    public Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 direction = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 previousPosition = new Vector3(0.0f, 0.0f, 0.0f); 
+    public Vector3 previousVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+    public float forceMagnitude = 0;
+       
+     void Start()
+    {
+        previousPosition = transform.position;
+    }
+    
     void Update()
     {
        
         velocity = transform.position - previousPosition; // 前フレームからの移動量を取得
         direction = velocity.normalized; // 移動方向を取得
-        direction.x = direction.x*10;
-        direction.y = direction.y*10;
-        direction.z = direction.z*10;
-        previousPosition = transform.position;
+
          // 前フレームからの速度変化を計算
         Vector3 velocityChange = velocity - previousVelocity;
 
@@ -33,27 +35,19 @@ public class PlayerFoot : MonoBehaviour
         // 加速度の計算
         Vector3 acceleration = velocityChange / deltaTime;
 
-     
-        // 推定される力の計算（近似的な値）
-        Vector3 force = 1 * acceleration;
-
         // ベクトルの大きさ（Magnitude）を取得し、それを float 型の変数に格納
-        forceMagnitude = force.magnitude;
+        forceMagnitude = acceleration.magnitude;
 
         // デバッグログに推定される力の大きさを表示
         
 
         // 現在の速度を前フレームの速度として保存
         previousVelocity = velocity;
-        // velocityやdirectionを使った処理
-            
+
+        direction *= 10;
+        previousPosition = transform.position;   
     }
-    
-     void Start()
-    {
-        previousPosition = transform.position;
-    }
-    
+
     
     // Triggerに他のColliderが入ったときに呼ばれる
     private void OnTriggerEnter(Collider other)
@@ -62,20 +56,10 @@ public class PlayerFoot : MonoBehaviour
 
 
         // Ballスクリプトを持っている物に触れた際に実行
-        Ball BallComponent = other.GetComponent<Ball>();
-        if (BallComponent != null)
+        if(other.GetComponent<Ball>())
         {
-            
-            Debug.Log("kick" );
-
             // 別のスクリプトを呼び出す
-            GameManager KickBall = FindObjectOfType<GameManager>();
-            if (KickBall != null)
-            {
-              Debug.Log("Velocity: " + forceMagnitude);
-              Debug.Log("direction: " + direction);
-                KickBall.KickBallToGoal(forceMagnitude*100,direction);
-            }
+            GameManager.instance.KickBallToGoal(forceMagnitude*100,direction);
         }
   }
 }
